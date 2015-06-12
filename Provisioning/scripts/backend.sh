eaff8ace7e3c4eb2167713752cf4cc1aeb1cf73e
Rodrigo Silva <rcandidosilva@gmail.com>
2015-06-12 02:06:37 -0300

    Changes on the provisioning scripts

--- Provisioning/scripts/backend.sh
+++ Provisioning/scripts/backend.sh
@@ -2,7 +2,7 @@
 
 cd /opt
 
-git clone -b prod https://github.com/unepwcmc/Taxonomic-Database-Application.git
+git clone -b release https://github.com/unepwcmc/Taxonomic-Database-Application.git
 
 cd ./Taxonomic-Database-Application
 #Update script for new releases
@@ -14,9 +14,13 @@ sudo chmod +x gradlew
 sudo ./gradlew build
 
 #Installing the backend application as Linux service
-sudo cp /vagrant/config/spring-boot /etc/init.d/taxonomic-db
+#sudo cp /vagrant/config/spring-boot /etc/init.d/taxonomic-db
+#sudo chmod 755 /etc/init.d/taxonomic-db
+#sudo chkconfig --add taxonomic-db
+#sudo mkdir /var/log/taxonomic-db
+cd ./build/libs
+sudo link -s ./taxonomic-db-spring-boot-0.0.1.SNAPSHOT.jar /etc/init.d/taxonomic-db
 sudo chmod 755 /etc/init.d/taxonomic-db
 sudo chkconfig --add taxonomic-db
-sudo mkdir /var/log/taxonomic-db
 
 service taxonomic-db start
