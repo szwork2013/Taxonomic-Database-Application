0fc83e1af51726762510639f5bc9f6e779c8d9a8
Rodrigo Silva <rcandidosilva@gmail.com>
2015-06-19 15:22:15 -0300

    Added occurrences species data

--- Backend/src/main/java/com/unep/wcmc/model/Occurrence.java
+++ Backend/src/main/java/com/unep/wcmc/model/Occurrence.java
@@ -2,37 +2,78 @@ package com.unep.wcmc.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 @Embeddable
 public class Occurrence implements Serializable {
 
     @Column
-    private Double latitude;
+    private String latitude;
 
     @Column
-    private Double longitude;
+    private String longitude;
+
+    @Column
+    private String projection;
 
     @ManyToOne
     @JoinColumn(name = "state_id")
     private State state;
 
-    public Double getLatitude() {
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "map_id")
+    private Map map;
+
+    public Occurrence() {
+        super();
+    }
+
+    public Occurrence(String latitude, String longitude, String projection, State state, Map map) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.projection = projection;
+        this.state = state;
+        this.map = map;
+    }
+
+    public String getLatitude() {
         return latitude;
     }
 
-    public void setLatitude(Double latitude) {
+    public void setLatitude(String latitude) {
         this.latitude = latitude;
     }
 
-    public Double getLongitude() {
+    public String getLongitude() {
         return longitude;
     }
 
-    public void setLongitude(Double longitude) {
+    public void setLongitude(String longitude) {
         this.longitude = longitude;
     }
+
+    public String getProjection() {
+        return projection;
+    }
+
+    public void setProjection(String projection) {
+        this.projection = projection;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
 }
--- Backend/src/main/java/com/unep/wcmc/repository/TaxonomyRepository.java
+++ Backend/src/main/java/com/unep/wcmc/repository/TaxonomyRepository.java
@@ -1,6 +1,17 @@
 package com.unep.wcmc.repository;
 
 import com.unep.wcmc.model.Taxonomy;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface TaxonomyRepository extends CrudRepository<Taxonomy, Long> {}
+import java.util.List;
+
+public interface TaxonomyRepository extends CrudRepository<Taxonomy, Long> {
+
+    Taxonomy findByHierarchySpeciesContaining(String species);
+
+    @Query(value = "select * from taxonomy where levenshtein(taxonomy.species, :species)  <= 2", nativeQuery = true)
+    List<Taxonomy> findByHierarchySpeciesSoundex(@Param("species") String species);
+
+}
