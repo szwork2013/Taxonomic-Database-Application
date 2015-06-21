b39d22a1f77a2df03e8fe809733c49776d33c156
Adriano Braga <adriano.alencar@integritas.com.br>
2015-06-21 19:03:51 -0300

    update import sql

--- Backend/src/main/resources/import.sql
+++ Backend/src/main/resources/import.sql
@@ -1,4 +1,4 @@
---BEGIN;
+BEGIN;
 
 -- ExtinctionRiskConfiguration data
 INSERT INTO extinction_risk_configuration (id, category, b2a_number_of_locations, c_number_of_mature_individuals, d_number_of_mature_individuals, c2a_number_of_mature_individuals_in_sub, b2_area_of_ocuppancy, b1_extent_of_occurrence, c2a_perc_mature_individuals_one_sub, a1_perc_population_decline, a2_perc_population_decline, e_prob_extinction_in_the_wild) VALUES (1, 'CRITICALLY_ENDANGERED', 1, 250, 50, 50, 10, 100, 90, 90.0, 80.0, 50.0);
@@ -5219,8 +5219,8 @@ INSERT INTO threat(id, per_affected_area, decline_in_number_of_locations, threat
 -- Conservation data
 INSERT INTO conservation(id, benefited_from_action_plan, convention, convention_other_relevant_data, ex_situ_management, other_actions_protect_species, action_plans, change_reasons, national_evaluation_elegible, necessary_research_for_conservation, previous_national_assessment, research_in_progress, specific_actions_in_protected_areas, type_id) VALUES (1, 'benefited_from_action_plan', 'convention', 'convention_other_relevant_data', 'ex_situ_management', 'other_actions_protect_species', 'action_plans', 'change_reasons', 'true', 'necessary_research_for_conservation', 'previous_national_assessment', 'research_in_progress', 'specific_actions_in_protected_areas', 1);
 -- Map data
-INSERT INTO map(id, auhtor, caption, date, description, file, is_cover, projection, type) VALUES (1, 'Brazil', 'Caption', '1999-01-08', 'Brazil', NULL, 'true', 'projection', 'local');
+INSERT INTO map(id, author, caption, date, description, file, set_as_cover, projection, type) VALUES (1, 'Brazil', 'Caption', '1999-01-08', 'Brazil', NULL, 'true', 'projection', 'local');
 -- Distribution area data
 INSERT INTO distribution_area(id, endemic_from_brasil, extent_of_occurrence, fragmentation_level, global_distribution, national_distribution, native_in_brazil, area_of_ocuppancy, occurrence_state, only_from_few_localities, the_region_is_well_sampled, trend_in_extent_of_occurrence, area_of_occupancy_trend) VALUES (1, 'true', 0, 0, 'true', 'true', 'true', 0, 'state', 'true', 'true', 0, 0);
 
---COMMIT;
+COMMIT;
