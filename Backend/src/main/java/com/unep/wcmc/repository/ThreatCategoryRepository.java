02f85f3aef1afa4b7a45022d1620f91d848898ab
Rodrigo Silva <rcandidosilva@gmail.com>
2015-06-23 21:59:39 -0300

    Added ThreatCategory repostory and service

---
+++ Backend/src/main/java/com/unep/wcmc/repository/ThreatCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.unep.wcmc.repository;
+
+import com.unep.wcmc.model.ThreatCategory;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ThreatCategoryRepository extends CrudRepository<ThreatCategory, Long> {}
