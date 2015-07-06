8fcf9217226a1cc00e8b0619c8e885412708165f
Rodrigo Silva <rcandidosilva@gmail.com>
2015-07-06 00:38:02 -0300

    Changes on the update scripts

--- update.sh
+++ update.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
 
+cd /opt/Taxonomic-Database-Application/
+
 echo "Stopping the WSO2 ESB..."
 sudo service wso2-esb stop
 
 echo "Stopping the PostgreSQL..."
 sudo service postgresql stop
 
+echo "Stopping the frontend..."
+sudo service taxonomic-web stop
+
 echo "Stopping the backend REST API..."
-sudo kill -9 $(pidof java)
+sudo service taxonomic-db stop 
 
 echo "Updating the project files..."
 sudo git pull
@@ -19,5 +24,10 @@ echo "Starting the WSO2 ESB..."
 sudo service wso2-esb start
 
 echo "Starting the backend REST API..."
+cd ./Backend
 sudo chmod +x ./gradlew
-sudo ./gradlew bootRun &
+sudo ./gradlew build -x test 
+sudo service taxonomic-db start
+
+echo "Starting the frontend..."
+sudo service taxonomic-web start  
