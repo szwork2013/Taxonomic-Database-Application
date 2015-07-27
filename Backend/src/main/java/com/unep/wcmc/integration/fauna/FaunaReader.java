cb74db59f9d530ee287cc08d2a5ff0eec91f367f
Rodrigo Silva <rcandidosilva@gmail.com>
2015-07-27 05:01:57 -0300

    Changes on the Taxonomic project

--- Backend/src/main/java/com/unep/wcmc/integration/fauna/FaunaReader.java
+++ Backend/src/main/java/com/unep/wcmc/integration/fauna/FaunaReader.java
@@ -6,7 +6,10 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.InputStreamReader;
 
 @Component
 public class FaunaReader implements ItemReader<String[]> {
@@ -21,7 +24,7 @@ public class FaunaReader implements ItemReader<String[]> {
         if (reader == null) {
             // CSV fields
             //Reino,Filo,Classe,Ordem,Familia,Genero,Epiteto Especifico,Subespecie,species,Nome cientifico,Nome Comum
-            reader = new CSVReader(new FileReader(CSV_DATA_FILE));
+            reader = new CSVReader(new BufferedReader(new InputStreamReader(new FileInputStream(CSV_DATA_FILE), "UTF-8")));
         }
         return reader.readNext();
     }
