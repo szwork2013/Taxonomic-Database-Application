295f84d5ad3a635d45eaf8fa71cd7fe341c8e120
Adriano Braga <adriano.alencar@integritas.com.br>
2015-08-12 14:29:50 -0300

    added enum missing fields added new algorithm fields

--- Backend/src/main/java/com/unep/wcmc/model/ExtinctionRiskCriteria.java
+++ Backend/src/main/java/com/unep/wcmc/model/ExtinctionRiskCriteria.java
@@ -13,17 +13,24 @@ public enum ExtinctionRiskCriteria {
     B1ac_i, B1ac_ii, B1ac_iii, B1ac_iv,
     B1b_i_c_i, B1b_i_c_ii, B1b_i_c_iii, B1b_i_c_iv,
     B1b_ii_c_i, B1b_ii_c_ii, B1b_ii_c_iii, B1b_ii_c_iv,
-    B1b_iii_c_i,
+    B1b_iii_c_i, B1b_iii_c_ii, B1b_iii_c_iii, B1b_iii_c_iv,
+    B1b_iv_c_i, B1b_iv_c_ii, B1b_iv_c_iii, B1b_iv_c_iv,
+    B1b_v_c_i, B1b_v_c_ii, B1b_v_c_iii, B1b_v_c_iv,
 
     // Criteria B2
     B2ab_i, B2ab_ii, B2ab_iii, B2ab_iv, B2ab_v,
-    B2ac_i, B2ac_ii, B2ac_iii,
-
+    B2ac_i, B2ac_ii, B2ac_iii, B2ac_iv,
+    B2b_i_c_i, B2b_i_c_ii, B2b_i_c_iii, B2b_i_c_iv,
+    B2b_ii_c_i, B2b_ii_c_ii, B2b_ii_c_iii, B2b_ii_c_iv,
+    B2b_iii_c_i, B2b_iii_c_ii, B2b_iii_c_iii, B2b_iii_c_iv,
+    B2b_iv_c_i, B2b_iv_c_ii, B2b_iv_c_iii, B2b_iv_c_iv,
+    B2b_v_c_i, B2b_v_c_ii, B2b_v_c_iii, B2b_v_c_iv,
+    
     // Criteria C
     C1, C2a_i, C2a_ii, C2b,
 
     // Criteria D
-    D,
+    D, D1, D2, D3,
 
     // Criteria E
     E
